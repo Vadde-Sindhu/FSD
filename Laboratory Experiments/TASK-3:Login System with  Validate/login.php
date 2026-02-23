60c665d4741f7632f257dfaf93e3cc1d9b296ae2
Vadde-Sindhu <vtu25871@veltech.edu.in>
2026-02-23 11:11:26 +0530

    login.php

---
+++ Laboratory Experiments/TASK-3:Login System with  Validate/login.php
@@ -0,0 +1,19 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "login_system");
+
+if (!$conn) {
+    die("Connection failed");
+}
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    echo "Login Successful ✅";
+} else {
+    echo "Invalid Username or Password ❌";
+}
+?>
